0ca089325974b5fb19731eedef4d520ec2d409d6
snajera1 <snajera@oakland.edu>
2024-09-17 16:14:32 -0400

    first commit

---
+++ HelloWorld/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");  //This line of code prints "Hello, World!" in the console and goes to the next line
+    }
+}
