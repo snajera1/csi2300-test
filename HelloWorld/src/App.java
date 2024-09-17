b5feeed3b8011cc55b8646012d9dfb6ddc1fa776
snajera1 <snajera@oakland.edu>
2024-09-17 16:25:10 -0400

    second commit

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");  //This line of code prints "Hello, World!" in the console and goes to the next line
+        System.out.println("Hello, CSI 2300!");  //This line of code prints "Hello, World!" in the console and goes to the next line
     }
 }
